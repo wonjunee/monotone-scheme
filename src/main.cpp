22f9c3ae6a51343d48298f8715cc6e6f0e4b8c31
Wonjun Lee <wlee10@g.ucla.edu>
2023-08-27 16:02:30 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/iostream.h>
 #include <pybind11/numpy.h>
-#include "affine3d.h"
+#include "Curvature2d.h"
+#include "Curvature3d.h"
 
 namespace py = pybind11;
 
-// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
 PYBIND11_MODULE(monotone, m) {
     // optional module docstring
     m.doc() = "C++ wrapper for monotone discretization code in 3D Cartesian grids setting";
 
-    py::class_<Affine3DSolver>(m, "Affine3DSolver") 
+    py::class_<Curv2DSolver>(m, "Curv2DSolver") 
         .def(py::init<py::array_t<double> &, py::array_t<int> &, int>())
-        .def("perform_one_iteration", &Affine3DSolver::perform_one_iteration);
+        .def("perform_one_iteration", &Curv2DSolver::perform_one_iteration);
+
+    py::class_<Curv3DSolver>(m, "Curv3DSolver") 
+        .def(py::init<py::array_t<double> &, py::array_t<int> &, int>())
+        .def("perform_one_iteration", &Curv3DSolver::perform_one_iteration);
 }
